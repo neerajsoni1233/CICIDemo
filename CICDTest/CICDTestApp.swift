ca85afe66aad54a99c984c6447aabed397fadb82
Neeraj <neeraj.jai1233@gmail.com>
2024-07-25 10:56:52 +0530

    init code

---
+++ CICDTest/CICDTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDTestApp.swift
+//  CICDTest
+//
+//  Created by Neeraj soni on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CICDTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
