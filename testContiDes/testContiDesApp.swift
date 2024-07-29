0c15cc58b0951d767578f6d0959d3ddd2170cab8
Neeraj <neeraj.jai1233@gmail.com>
2024-07-29 12:32:37 +0530

    new app

--- testContiDes/testContiDesApp.swift
+++ testContiDes/testContiDesApp.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 import AppCenter
 import AppCenterAnalytics
-import AppCenterCrashes
+import AppCenterCrashes   //23f43fc7-9223-4c5c-9bb5-01824a88aa89
 
 @main
 struct testContiDesApp: App {
     init() {
-               AppCenter.start(withAppSecret: "23f43fc7-9223-4c5c-9bb5-01824a88aa89", services: [
+               AppCenter.start(withAppSecret: "26d7eb73-82ab-4d35-8a58-ec08e313aa23", services: [
                    Analytics.self,
                    Crashes.self
                ])
